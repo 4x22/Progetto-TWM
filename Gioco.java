a4b337298295c020c9c15b5976e0f010a7321d1b
Antonio Dal Cin <antoniox22@live.it>
2013-12-13 12:31:47 +0100

    Aggiunti commenti

--- Gioco.java
+++ Gioco.java
@@ -3,37 +3,42 @@ import java.io.*;
 class Gioco {
     public static Tappa[] tappe;
 	public static void main(String[] args) throws Exception{
-        //System.out.println("Inserisci il nome del file da leggere:");
+        System.out.println("Inserisci il nome del file da leggere:");
 		try {
-        	FileReader fileReader = new FileReader("Avventura.txt"/*Leggi.unoString()*/);
+            //Inizializzo fileReader e bufferedReader per leggere il file testuale, di cui il nome è dato in ingresso
+        	FileReader fileReader = new FileReader(Leggi.unoString());
 		    BufferedReader bufferedReader = new BufferedReader(fileReader);
 
+            //Inizializzo le variabili relative alle informazioni generali delle tappe 
             int totaleTappe, numeroTappa, numeroRighe;
             int[] tappeRaggiungibili;
             String riga = "";
 
+            //Dichiaro l'array di tappe con le dimensioni adatte a contenere tutte le tappe
             totaleTappe = Integer.parseInt(bufferedReader.readLine());
             tappe = new Tappa[totaleTappe];
 
+            //Utilizzo un ciclo di tipo FOR per riempire l'aray di tappe
             for(int i = 0; i < tappe.length; i++) {
-            	numeroTappa = Integer.parseInt(bufferedReader.readLine());
 
+                //Inizializzo delle variabili conteneti informazioni sulla singola tappa e utilizzando il parser otteniamo le informazioni dal file testuale
+            	numeroTappa = Integer.parseInt(bufferedReader.readLine());
                 numeroRighe = Integer.parseInt(bufferedReader.readLine());
                 for(int j = 0; j < numeroRighe; j++) {
                     riga += bufferedReader.readLine() + "\n";
                 }
-
                 numeroRighe = Integer.parseInt(bufferedReader.readLine());
                 tappeRaggiungibili = new int[numeroRighe];
                 for(int j = 0; j < tappeRaggiungibili.length; j++) {
                     tappeRaggiungibili[j] = Integer.parseInt(bufferedReader.readLine());
                 }
 
+                //E' finalmente dichiarata ogni singola tappa
                 tappe[i] = new Tappa(numeroTappa, riga, tappeRaggiungibili);
 
                 riga = "";
             }
-            
+            //Chiusura del fileReader
             fileReader.close();
 
             for(int i = 0; i < tappe.length; i++) {
